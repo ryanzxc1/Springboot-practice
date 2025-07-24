ba6efa272c3a7bd1e3d778d0d34979cc6dabe0a7
ryanzxc1 <ryan17126@naver.com>
2025-07-24 21:14:48 +0900

    Commit1

---
+++ src/main/java/com/example/testprojectspringboot/TestProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.testprojectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testprojectspringboot/TestProjectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testprojectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
